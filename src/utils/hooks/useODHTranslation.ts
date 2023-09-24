6c75a9f0e80cbf4d019f54bc726dcf578d26cde0
Trevor Royer <trevorroyer@gmail.com>
2023-09-23 19:47:04 -0600

    odh-plugin to console-plugin

--- src/utils/hooks/useODHTranslation.ts
+++ src/utils/hooks/useODHTranslation.ts
@@ -1,6 +1,7 @@
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
 
 /**
  * A Hook for using the i18n translation.
  */
-export const useODHTranslation = () => useTranslation("plugin__odh-plugin");
+export const useODHTranslation = () =>
+  useTranslation('plugin__odh-console-plugin');
